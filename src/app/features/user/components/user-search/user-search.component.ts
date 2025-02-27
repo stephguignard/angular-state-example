54f2855b898f7ed6a0fe60f68bf587c2f66b622d
Stephane Guignard <stephguignard@gmail.com>
2025-02-27 21:57:33 +0100

    feature: Add primeng

--- src/app/features/user/components/user-search/user-search.component.ts
+++ src/app/features/user/components/user-search/user-search.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-user-search',
-  imports: [],
+  imports: [
+    Button
+  ],
   templateUrl: './user-search.component.html',
   styleUrl: './user-search.component.scss'
 })
--- src/app/features/user/components/user-table/user-table.component.ts
+++ src/app/features/user/components/user-table/user-table.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TableModule} from 'primeng/table';
+import {User} from '../../models/user';
+import {UserStateService} from '../../services/user-state.service';
 
 @Component({
   selector: 'app-user-table',
-  imports: [],
+  imports: [
+    TableModule
+  ],
+  providers: [UserStateService],
   templateUrl: './user-table.component.html',
   styleUrl: './user-table.component.scss'
 })
-export class UserTableComponent {
+export class UserTableComponent implements OnInit {
+  users!: User[];
+
+  constructor(private userStateService:UserStateService) {
+  }
+
+  ngOnInit() {
+    this.users=[
+      {
+        name:'tata',
+        firstName:'stephane',
+        email:'sguik@gmail.com'
+      },
+      {
+        name:'Guignard',
+        firstName:'patrick',
+        email:'pauik@gmail.com'
+      }
+      ]
+  }
 
 }
