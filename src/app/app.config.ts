f64984a6d0b0aefdf5cc442817f540b02dd99cff
Stephane Guignard <stephguignard@gmail.com>
2025-02-28 21:32:11 +0100

    feature: Add tailwind 4

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,34 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {providePrimeNG} from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import Lara from '@primeng/themes/lara';
+import {definePreset} from '@primeng/themes';
+
+// Définir un preset personnalisé en étendant Lara
+const CustomPreset = definePreset(Lara, {
+  // Définir des couleurs primitives personnalisées
+  primitive: {
+    customPrimary: {
+      500: '#FF5733', // Remplacez par la couleur hexadécimale souhaitée
+    },
+  },
+  // Mapper les couleurs sémantiques sur les couleurs primitives
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}'
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,7 +40,13 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura
+        preset: CustomPreset,
+        options: {
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng'
+          }
+        }
       }
     })
   ]
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,scss}" // Indique où chercher les classes Tailwind
+  ],
+  theme: {
+    extend: {},
+  },
+};
