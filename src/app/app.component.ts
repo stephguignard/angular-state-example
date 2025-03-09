e890f1373bd067a2ba558f10e669edad410de9fd
Stephane Guignard <stephguignard@gmail.com>
2025-03-09 08:54:09 +0100

    feature: Create MenuBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Menubar} from 'primeng/menubar';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menubar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-state-example';
+export class AppComponent implements OnInit {
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        routerLink: '/home',
+      },
+      {
+        label: 'User',
+        icon: 'pi pi-user',
+        routerLink: '/user',
+      },
+      {
+        label: 'ToDo',
+        icon: 'pi pi-envelope',
+        routerLink: '/todo',
+      }
+    ]
+  }
+
 }
--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import {Card} from 'primeng/card';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [
+    Card,
+    RouterLink
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+
 
 }
